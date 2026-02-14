ca5dbf2130fbdca4ff875a56c2b728559fac3e6a
Olga Varanytsia <oliavaranytsaa@gmail.com>
2026-02-14 13:39:35 +0200

    Fixed structure to core and sleep_tracker

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
+    'sleep_tracker',
 ]
 
 MIDDLEWARE = [
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'myproject.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myproject.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
