69a0f8412ce8a52c90aa295b78f6648853c01eba
Olga Varanytsia <oliavaranytsaa@gmail.com>
2026-02-21 00:19:50 +0200

    minor fixes

--- sleep_tracker/models.py
+++ sleep_tracker/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 class SleepRecord(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     created_at = models.DateField()
@@ -11,10 +11,10 @@ class SleepRecord(models.Model):
         return f"{self.user} {self.created_at}: {self.duration:.2f} годин."
 
     @property
-    def duration (self):
+    def duration(self):
         """sleep duration."""
         delta = self.end_time - self.start_time
-        return max (delta.total_seconds() / 3600, 0)
+        return max(delta.total_seconds() / 3600, 0)
 
 
 
