28cc04b4bbd071b2eb0273316d5ae06c2f1bba9f
Olga Varanytsia <oliavaranytsaa@gmail.com>
2026-02-26 21:01:33 +0200

    healthstats

--- core/settings.py
+++ core/settings.py
@@ -37,10 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sleep_tracker',
     'user',
     'nutrition',
-    'healthstats'
+    'healthstats',
 
 ]
 
